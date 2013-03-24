207144746221b2bed5b84c1ff146ab098cae0949
Alexandre HOARAU <alexandre.hoarau974@gmail.com>
2013-03-24 23:33:22 +0400

    Suppression du Javascript Non-utilisé

--- Web/contact.php
+++ Web/contact.php
@@ -12,58 +12,7 @@
 		<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.js"></script>
 <script type="text/javascript" src="toastr.js"></script>
 <link rel="Stylesheet" href="toastr.css" />
-		<script type="text/javascript">
-		//Code Javascript
-		$(function() {
-	grid = $('#user');
 
-	// handle search fields key up event
-	$('#search-term').keyup(function(e) { 
-		text = $(this).val(); // grab search term
-
-		if(text.length > 1) {
-			grid.find('tr:has(td)').hide(); // hide data rows, leave header row showing
-
-			// iterate through all grid rows
-			grid.find('tr').each(function(i) {
-				// check to see if search term matches Name column
-				if($(this).find('td:first-child').text().toUpperCase().match(text.toUpperCase()))
-					$(this).show(); // show matching row
-			});
-		}
-		else 
-			grid.find('tr').show(); 
-
-	});
-});	
-		</script>
-<script>
-    $(function() {
-      $("#refresh").click(function(evt) {
-         $("#randomdiv").load("index.php")
-         evt.preventDefault();
-      })
-    })
-</script>
-	</head>
-
-	<body>
-<script>
-$(function() {
-  $("#scan").click(function() {
-     $("#info").load("info_table.php");
-     
-  })
-})
-</script>
-<script>
-$(function() {
-  $("#remove").click(function() {
-     $("#info").load("info_table.php");
-     
-  })
-})
-</script>
 
     <div id="test">
 	<div id="header">
