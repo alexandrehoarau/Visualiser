d3248a3abb763f2a8845f6fb0c8a113a648f9caa
Alexandre HOARAU <alexandre.hoarau974@gmail.com>
2013-03-25 16:55:18 +0400

    Ajout des classes et div pour le design

--- Web/formmail.php
+++ Web/formmail.php
@@ -1,13 +1,32 @@
-<!DOCTYPE html>
+
 <html>
-<?php include ('head.php');?>
-  </head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	
+	<head>
+		<link href="http://fonts.googleapis.com/css?family=Ubuntu:bold" rel="stylesheet" type="text/css">
+		<link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+		<script src=" sorttable.js "></script> 
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.js"></script>
+<script type="text/javascript" src="toastr.js"></script>
+<link rel="Stylesheet" href="toastr.css" />
+    <div id="test">
+	<div id="header">
+		<div id='cssmenu'>
+			<div id="header-title">
+			Visualiser
+			</div>
+			<ul>
+			   <li><a href='home.php'><span>Home</span></a></li>
+			    <li><a href='contact.php'><span>Contact</span></a></li>
+			 
+			</ul>
+		</div>
 
-  <body>
-<?php include('nav.php');?>
 
-    <div class="container">
-      <div class="hero-unit">
+	</div>
 
   		<?php
 			if(!empty($_POST['nom'])&&!empty($_POST['prenom'])&&!empty($_POST['mail'])&&!empty($_POST['message'])){
@@ -27,16 +46,18 @@
 					mail($to, $subject, $message,$header);
 
 					echo 'Votre message à été envoyé';
+					echo '</br><a href ="home.php">Retour Acceuil</a>';	
 
 					}else{
-					echo '<a href ="mail.php">';				//Si non, on retourne un message d'erreur qui indique à l'utilisateur qu'il na pas remplis tous les champs
-					echo '<div class="pull-center"><h2>Vous n\'avez pas rempli tout les champs!!</h2></div>';
+					echo '<a href ="contact.php">';				//Si non, on retourne un message d'erreur qui indique à l'utilisateur qu'il na pas remplis tous les champs
+					echo '<div class="pull-center"><h2>Vous n\'avez pas rempli tout les champs!!</h2></div></a>';
 				}
 			?>
+	
 		</div>
+	<div class='footer'>
+			Copyright © All Rights Reserved - HOARAU Alexandre & TETIA THOMAS
 	</div>
-</body>
 
-    <?php include('footer.php');?>
-    
+</body>
 </html>
